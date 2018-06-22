dbcf2a7ed96bafc188c36ca2b6d5f4f06ef34a0b
wpsaputra <wayanpermana93@gmail.com>
2018-06-22 14:00:18 +0800

    fix timeline todo remove layout header footer

--- views/master/view.php
+++ views/master/view.php
@@ -8,8 +8,8 @@ use yii\helpers\ArrayHelper;
 /* @var $model app\models\Master */
 
 $this->title = $model->kode_qr_code;
-$this->params['breadcrumbs'][] = ['label' => 'Masters', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+// $this->params['breadcrumbs'][] = ['label' => 'Masters', 'url' => ['index']];
+// $this->params['breadcrumbs'][] = $this->title;
 
 $this->registerJsFile(
     '@web/js/vertical-timeline.min.js',
@@ -25,9 +25,9 @@ $js = "$('#myTimeline').verticalTimeline({
 
 $this->registerJS($js);
 
-$array_posisi = array(1=>'Penerimaan TU', 2=>'Penerimaan IPDS', 3=>'Entri Dokumen', 4=>'Validasi Dokumen', 5=>'-');
+$array_posisi = array(1=>'Penerimaan TU', 2=>'Penerimaan IPDS', 3=>'Entri Dokumen', 4=>'Validasi Dokumen', 5=>'-', 6=>'Dokumen Belum Diterima');
 $array_quote = array(1=>'Tahapan penerimaan dokumen SUTAS dari kabupaten oleh TU Provinsi', 2=>'Tahapan penerimaan dokumen SUTAS dari TU Provinsi ke IPDS Provinsi', 
-3=>'Tahapan pengentrian dokumen SUTAS', 4=>'Tahapan validasi dokumen SUTAS', 5=>'-');
+3=>'Tahapan pengentrian dokumen SUTAS', 4=>'Tahapan validasi dokumen SUTAS', 5=>'-', 6=>'Belum dilakukan tahapan penerimaan dokumen');
 
 $timeline_template = '<div>
 <div class="date pull-right">?date?</div>
@@ -38,6 +38,12 @@ $timeline_template = '<div>
 
 // print_r(ArrayHelper::toArray($model_batch));
 ?>
+
+<div class="alert alert-info alert-dismissible" role="alert">
+    <!-- <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button> -->
+    <h2><strong>Status Posisi Batch <?=$model->kode_qr_code?></strong></pH1>
+</div>
+
 <div class="master-view">
 
     <div id="myTimeline">
@@ -48,6 +54,14 @@ $timeline_template = '<div>
                 $temp = str_replace("?date?", DateTime::createFromFormat('Y-m-d H:i:s', $value['date_terima'])->format('d M Y (H:i:s)'), $temp);
                 $temp = str_replace("?posisi?", $array_posisi[$value['id_posisi']], $temp);
                 $temp = str_replace("?quote?", $array_quote[$value['id_posisi']], $temp);
+                echo $temp;
+            }
+
+            if(count($arr_model_batch)==0){
+                $temp = $timeline_template;
+                $temp = str_replace("?date?", DateTime::createFromFormat('Y-m-d H:i:s', date("Y-m-d H:i:s"))->format('d M Y (H:i:s)'), $temp);
+                $temp = str_replace("?posisi?", $array_posisi[6], $temp);
+                $temp = str_replace("?quote?", $array_quote[6], $temp);
                 echo $temp;
             }
 
